e991ab83cd6352390dc85496809649cba3e241d8
swan0sasha <swan.sasha@gmail.com>
2023-12-21 20:06:00 +0700

    main panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CentralPanel from "./components/CentralPanel";
 import Top from "./components/Top";
 
 function App() {
   return (
     <div className="App">
       <Top></Top>
+      <CentralPanel></CentralPanel>
     </div>
   );
 }
