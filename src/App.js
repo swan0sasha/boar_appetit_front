954c2dc5a34440a98059139add9faa10dee01821
FacelessLake <sinonim147@gmail.com>
2023-12-21 20:19:05 +0700

    (Re)added Main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import CentralPanel from "./components/CentralPanel";
-import Top from "./components/Top";
 import {Routes, Route, Link} from 'react-router-dom'
 import {RecipePage} from './pages/RecipePage'
+import {MainPage} from './pages/MainPage'
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path={"/"} element={<Top/>}/>
+                <Route path={"/"} element={<MainPage/>}/>
                 <Route path={"/recipes/*"} element={<RecipePage/>}/>
             </Routes>
         </div>
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CentralPanel from "../components/CentralPanel";
+import Top from "../components/Top";
+
+function MainPage() {
+    return (
+        <div>
+            <Top/>
+            <CentralPanel/>
+        </div>
+    )
+}
+export {MainPage}
